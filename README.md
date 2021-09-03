e9b5ac83acd19c194d384ec1e4d03400ed478d7c
Wulan Mantiri <wulan.mantiri@gmail.com>
2021-09-03 19:00:44 +0800

    Integrate SAM and CI/CD for lambda function

---
+++ README.md
@@ -0,0 +1,25 @@
+# Smart Traffic Management System API
+
+## Getting Started
+
+1. Install [AWS SAM CLI](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install.html).
+2. Clone the repository and enter the project directory with `cd smart-traffic-api`.
+3. Build the dependencies with `sam build`.
+4. Run the api with `sam local start-api -p 8000`.
+5. Open [localhost:8000](http://localhost:8000) in apps like Postman.
+
+To learn AWS SAM, check out the [AWS SAM documentation](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/what-is-sam.html).
+
+## Deployment: CI/CD with AWS
+
+### S3
+- Create an S3 bucket with the default configurations.
+
+### CodeBuild
+- Create a build project and connect to github repository. 
+- Enable webhook to rebuild as code changes and add `PUSH` event types.
+- Select `Managed image` as environment image with `Amazon Linux 2` OS and `x86_64:3.0` image. Use or create new service role.
+- In `additional configuration`, add `SAM_S3_BUCKET_NAME`, `CLOUDFORMATION_STACK_NAME` and `REGION` values in environment variables. Optionally, set compute to the lowest spec.
+- Leave everything as is for easy configuration.
+
+After setup, every push to `master` will trigger CI/CD.
---
+++ functions/decide_traffic_light.py
@@ -0,0 +1,100 @@
+import json
+import boto3
+import os
+
+
+RESPONSE_HEADERS =  {
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+}
+
+
+def format_error_resp(error_message, status_code=400):
+    return {
+        'statusCode': status_code,
+        'headers': RESPONSE_HEADERS,
+        'body': json.dumps({
+            'error': error_message,
+        }),
+    }
+
+
+def format_success_resp(response_body, status_code=200):
+    return {
+        'statusCode': status_code,
+        'headers': RESPONSE_HEADERS,
+        'body': json.dumps(response_body),
+    }
+
+
+def handler(event, _):
+    try:
+        event_body = json.loads(event['body'])
+        lanes = event_body['lanes']
+        green_lane_history = event_body['green_lane_history']
+        config = event_body['config']
+    except KeyError as e:
+        return format_error_resp(f'Payload [{str(e)}] is required.')
+    except Exception as e:
+        return format_error_resp(f'Error {str(e)} occurred.')
+
+    min_green_time = config.get('min_green_time', 10)
+    max_green_time = config.get('max_green_time', 60)
+    ratio_time = config.get('ratio_time', 1)
+    ratio_vehicle = config.get('ratio_vehicle', 1)
+    enable_stream = config.get('enable_stream', True)
+
+    max_score = 0
+    max_prob = 0
+    green_lane = None
+    num_of_lanes = len(lanes)
+    score_details = []
+    for lane_id in range(num_of_lanes):
+        try:
+            prob = (green_lane_history.index(lane_id) + 1) / num_of_lanes
+        except:
+            prob = 1
+
+        vehicle_count = lanes[lane_id]['count']
+        score = prob * vehicle_count
+        if score > max_score or (score == max_score and prob > max_prob):
+            max_score = score
+            max_prob = prob
+            green_lane = lane_id
+
+        score_details.append(f'{prob:.2f} ✕ {vehicle_count} = {score:.2f}')
+
+    green_lane_count = lanes[green_lane]['count']
+    actual_green_time = round(green_lane_count * ratio_time / ratio_vehicle)
+    green_time = max([actual_green_time, min_green_time])
+    green_time = min([green_time, max_green_time])
+    green_lane_details = {
+        'lane': green_lane,
+        'calc_time': green_time,
+        'actual_time_details': f'{green_lane_count} ✕ ({ratio_time}/{ratio_vehicle}) = {actual_green_time}'
+    }
+
+    if green_lane in green_lane_history:
+        green_lane_history.remove(green_lane)
+    green_lane_history.insert(0, green_lane)
+
+    # Store data in S3 via Kinesis Firehose
+    if enable_stream:
+        firehose_client = boto3.client('firehose')
+        stream_name = os.environ.get('DELIVERY_STREAM_NAME')
+        firehose_client.put_record_batch(
+            DeliveryStreamName=stream_name,
+            Records=[
+                {
+                    'Data': json.dumps(lanes)
+                },
+            ]
+        )
+
+    response_body = {
+        'score_details': score_details,
+        'green': green_lane_details,
+        'green_lane_history': green_lane_history,
+    }
+    return format_success_resp(response_body)
